13a676f11a605fbadfed6445c430b90a2ba4adb5
Codingclap <codingclap@gmail.com>
2023-04-14 18:48:34 +0530

    Final fixes

--- js/scripts.js
+++ js/scripts.js
@@ -1,81 +1,81 @@
-jQuery(document).ready(function(jQuery) {
-      jQuery('.slick-carousel').slick({
-        dots: false,
-        infinite: true,
-        speed: 1500,
+jQuery(document).ready(function (jQuery) {
+  jQuery('.slick-carousel').slick({
+    dots: false,
+    infinite: true,
+    speed: 1500,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 1500,
+    arrows: true,
+    prevArrow: '<div class="position-relative"><div class="slick-nav  prev-arrow"><img class="mt-2" src="img/dark-arrow-left.svg"></div></div>',
+    nextArrow: '<div class="position-relative"><div class="slick-nav next-arrow"><img class="mt-2" src="img/dark-arrow-right.svg"></div></div>',
+    responsive: [{
+      breakpoint: 600,
+      settings: {
         slidesToShow: 1,
         slidesToScroll: 1,
-		autoplay: true,
-        autoplaySpeed: 1500,
-        arrows: true,
-		prevArrow: '<div class="position-relative"><div class="slick-nav  prev-arrow"><img class="mt-2" src="img/dark-arrow-left.svg"></div></div>',
-		nextArrow: '<div class="position-relative"><div class="slick-nav next-arrow"><img class="mt-2" src="img/dark-arrow-right.svg"></div></div>',
-        responsive: [{
-          breakpoint: 600,
-          settings: {
-            slidesToShow: 1,
-            slidesToScroll: 1,
-			prevArrow: false,
-			nextArrow: false,
-			 arrows: false
-          }
-        },
-        {
-           breakpoint: 400,
-           settings: {
-              arrows: false,
-              slidesToShow: 1,
-              slidesToScroll: 1,
-			  prevArrow: false,
-			nextArrow: false,
-			 arrows: false
-           }
-        }]
-    }); 
-	
-	/**testimonial **/
-      jQuery('.whole-test').slick({
-        dots: false,
-        infinite: true,
-        speed: 1500,
+        prevArrow: false,
+        nextArrow: false,
+        arrows: false
+      }
+    },
+    {
+      breakpoint: 400,
+      settings: {
+        arrows: false,
         slidesToShow: 1,
         slidesToScroll: 1,
-		autoplay: true,
-        autoplaySpeed: 1500,
+        prevArrow: false,
+        nextArrow: false,
+        arrows: false
+      }
+    }]
+  });
+
+  /**testimonial **/
+  jQuery('.whole-test').slick({
+    dots: false,
+    infinite: true,
+    speed: 1500,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 1500,
+    arrows: true,
+
+    responsive: [{
+      breakpoint: 1000,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1,
+
+        arrows: false,
+      }
+    },
+    {
+      breakpoint: 1400,
+      settings: {
+        arrows: false,
+        slidesToShow: 1,
+        slidesToScroll: 1,
+
         arrows: true,
+      }
+    }]
+  });
 
-        responsive: [{
-          breakpoint: 1000,
-          settings: {
-            slidesToShow: 1,
-            slidesToScroll: 1,
-
-			 arrows: false,
-          }
-        },
-        {
-           breakpoint: 1400,
-           settings: {
-              arrows: false,
-              slidesToShow: 1,
-              slidesToScroll: 1,
-
-			 arrows: true,
-           }
-        }]
-    }); 
-
-	// jQuery('.we-are').slick({
+  // jQuery('.we-are').slick({
   //       dots: false,
   //       infinite: true,
   //       speed: 1200,
   //       slidesToShow: 1,
   //       slidesToScroll: 1,
-	// 	autoplay: true,
+  // 	autoplay: true,
   //       autoplaySpeed: 2500,
   //       arrows: true,
-	// 	prevArrow: '<div class="position-relative"><div class="slick-nav  prev-arrow"><img class="mt-2" src="img/arrow-up.svg"></i></div></div>',
-	// 	nextArrow: '<div class="position-relative"><div class="slick-nav next-arrow"><img class="mt-2" src="img/arrow-down.svg"></i></div></div>',
+  // 	prevArrow: '<div class="position-relative"><div class="slick-nav  prev-arrow"><img class="mt-2" src="img/arrow-up.svg"></i></div></div>',
+  // 	nextArrow: '<div class="position-relative"><div class="slick-nav next-arrow"><img class="mt-2" src="img/arrow-down.svg"></i></div></div>',
   //       responsive: [{
   //         breakpoint: 600,
   //         settings: {
@@ -93,54 +93,54 @@ jQuery(document).ready(function(jQuery) {
   //       }]
   //   });
 
-jQuery('.center-slider').slick({
-  centerMode: true,
-  centerPadding: '20%',
-  slidesToShow: 1,
+  jQuery('.center-slider').slick({
+    centerMode: true,
+    centerPadding: '20%',
+    slidesToShow: 1,
     speed: 1000,
-		prevArrow: '<div class="position-relative"><div class="slick-nav  prev-arrow"><img class="w-100" src="img/arrow-left.svg"></div></div>',
-		nextArrow: '<div class="position-relative"><div class="slick-nav next-arrow"><img class="w-100" src="img/arrow-right.svg"></div></div>',
+    prevArrow: '<div class="position-relative"><div class="slick-nav  prev-arrow"><img class="w-100" src="img/arrow-left.svg"></div></div>',
+    nextArrow: '<div class="position-relative"><div class="slick-nav next-arrow"><img class="w-100" src="img/arrow-right.svg"></div></div>',
+    autoplaySpeed: 3000,
+    autoplay: true,
+
+    responsive: [
+      {
+        breakpoint: 768,
+        settings: {
+          arrows: false,
+          centerMode: true,
+          centerPadding: '40px',
+          slidesToShow: 1,
+          speed: 1000,
           autoplaySpeed: 3000,
-        autoplay: true,
-        
-  responsive: [
-    {
-      breakpoint: 768,
-      settings: {
-        arrows: false,
-        centerMode: true,
-        centerPadding: '40px',
-        slidesToShow: 1,
-		  speed: 1000,
-		        autoplaySpeed: 3000,
-        autoplay: true
-		
+          autoplay: true
+
+        }
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          arrows: false,
+          centerMode: true,
+          centerPadding: '40px',
+          slidesToShow: 1,
+          speed: 1000,
+          autoplaySpeed: 3000,
+          autoplay: true
+        }
       }
-    },
-    {
-      breakpoint: 480,
-      settings: {
-        arrows: false,
-        centerMode: true,
-        centerPadding: '40px',
-        slidesToShow: 1,
-		  speed: 1000,
-		        autoplaySpeed: 3000,
-        autoplay: true
-      }
-    }
-  ]
-});
+    ]
+  });
 
 
- jQuery('.carouse').slick({
+  jQuery('.carouse').slick({
     speed: 500,
     slidesToShow: 4,
     slidesToScroll: 1,
     autoplay: true,
     autoplaySpeed: 2000,
-    dots:false,
-	  arrows: false,
+    dots: false,
+    arrows: false,
     centerMode: true,
     responsive: [{
       breakpoint: 1024,
@@ -154,20 +154,20 @@ jQuery('.center-slider').slick({
       breakpoint: 800,
       settings: {
         slidesToShow: 1,
-		  arrows: false,
+        arrows: false,
         slidesToScroll: 4,
         dots: false,
         infinite: true,
 
       }
-    },  {
+    }, {
       breakpoint: 480,
       settings: {
         slidesToShow: 1,
         slidesToScroll: 1,
         dots: false,
         infinite: true,
-		  arrows: false,
+        arrows: false,
         autoplay: true,
         autoplaySpeed: 2000,
       }
